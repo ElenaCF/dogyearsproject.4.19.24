a4ac089591f87343fa31588618d11988706060c6
Elena <139893723+ElenaCF@users.noreply.github.com>
2024-04-19 19:00:40 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// The user's age is stored in the "myAge" variable.
+const myAge = 24;
+// earlyYears variable
+let earlyYears = 2;
+// earlyYears calculations
+earlyYears *= 10.5;
+// Declaring the laterYears variable using the myAge variable.
+let laterYears = myAge - 2;
+// Calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+// End result of the code. It adds combines the addition result of early years and the addition result of later years.
+const myAgeInDogYears = earlyYears + laterYears;
+// My name is stored in the myName variable.
+const myName = "Elena";
+// Outputs your name and age in dog years.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`);
